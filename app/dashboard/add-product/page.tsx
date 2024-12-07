874bcf8c2fb433a806166da4ff0153513fcf5d76
phanmai03 <inet.ngocmai.v6@gmail.com>
2024-12-07 14:20:48 +0700

    add order , delivery and coupon

--- app/dashboard/add-product/page.tsx
+++ app/dashboard/add-product/page.tsx
@@ -77,6 +77,8 @@ const Page = () => {
             <Images
               formData={formData}
               setFormData={setFormData}
+              userId={userId}
+              accessToken={accessToken}
             />
 
 
