c08343c699fe298d563f94565106434866773815
phanmai03 <inet.ngocmai.v6@gmail.com>
2024-12-07 20:47:51 +0700

    .

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -6,15 +6,10 @@ import logo from "../../public/logo.svg";
 import {
   LayoutGrid,
   Users2,
-  UserSquare2,
-  Mail,
   TicketPercent,
-  MessagesSquare,
   ShoppingBasket,
   ChartNoAxesCombined,
   HousePlus,
-  Settings,
-  ListTodo,
   Truck,
   ShoppingBag
 } from "lucide-react";
@@ -33,15 +28,10 @@ const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
     { title: "Category", icon: LayoutGrid, href: "/dashboard/category" },
     { title: "Add Product", icon: HousePlus, href: "/dashboard/add-product" },
     { title: "All Product", icon: ShoppingBasket, href: "/dashboard/all-product" },
-    // { title: "Email", icon: Mail, href: "/dashboard/email" },
     { title: "Coupon", icon: TicketPercent, href: "/dashboard/coupon" },
-    // { title: "Feedback", icon: MessagesSquare, href: "/dashboard/feedback" },
-    // { title: "Blog", icon: ListTodo, href: "/dashboard/blog" },
-    // { title: "Seller", icon: UserSquare2, href: "/dashboard/seller" },
     { title: "User", icon: Users2, href: "/dashboard/user" },
     { title: "Delivery", icon: Truck, href: "/dashboard/delivery" },
     { title: "Orders", icon: ShoppingBag, href: "/dashboard/orders" },
-    // { title: "Return Policy", icon: Settings, href: "/dashboard/return-policy" },
   ];
 
 
